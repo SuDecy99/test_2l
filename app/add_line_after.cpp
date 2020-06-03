2e23316288d60cb40514c9416024bb0bea4664ab
SuDecy99 <62535336+SuDecy99@users.noreply.github.com>
2020-06-03 15:10:52 +0300

    q

---
+++ app/add_line_after.cpp
@@ -0,0 +1,17 @@
+#include "_text.h"
+
+/**
+ * Добавляет одну строку к тексту после строки с указанным номером
+ * @param txt текст
+ * @param line_number номер после которого нажо вставить строку
+ * @param to_add текст новой строки
+ * @returns статус
+ */
+int add_line_after(const text txt, int line_number, string to_add)
+{
+    list<string>::iterator it = txt->lines->begin();
+    advance(it, line_number + 1);
+    txt->lines->insert(it, to_add);
+
+    return SUCCESS;
+}
